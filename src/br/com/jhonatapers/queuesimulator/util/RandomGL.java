eaba81256dafe949948d8f79cc844d65ed5c5e52
Henrique Oliveira <h.oliveira@edu.pucrs.br>
2023-03-15 19:24:14 -0300

    Remove predefinidos

--- src/br/com/jhonatapers/queuesimulator/util/RandomGL.java
+++ src/br/com/jhonatapers/queuesimulator/util/RandomGL.java
@@ -14,13 +14,8 @@ public class RandomGL {
 
     private Long seed;
 
-    private Queue<Float> preDefinidos;
-
     public RandomGL() {
         this.seed = System.nanoTime();
-        this.preDefinidos = new LinkedList<Float>(Arrays.asList(
-            0.0176F, 0.1103F, 0.2109F, 0.3456F, 0.4910F, 0.5323F, 0.6211F, 0.7322F, 0.8211F, 0.9131F, 0.1208F, 0.2172F,
-            0.3922F, 0.4324F, 0.5011F, 0.6931F));
     }
 
     public RandomGL(Long seed) {
@@ -28,15 +23,11 @@ public class RandomGL {
     }
 
     public void novaSeed(Long seed) {
-        this.preDefinidos = new LinkedList<Float>(Arrays.asList(
-            0.0176F, 0.1103F, 0.2109F, 0.3456F, 0.4910F, 0.5323F, 0.6211F, 0.7322F, 0.8211F, 0.9131F, 0.1208F, 0.2172F,
-            0.3922F, 0.4324F, 0.5011F, 0.6931F));
         this.seed = seed;
     }
 
     public Float nextRandom() {
-        return preDefinidos.poll();
-        //return Float.parseFloat(random(seed).toString()) / Float.parseFloat(M.toString());
+        return Float.parseFloat(random(seed).toString()) / Float.parseFloat(M.toString());
     }
 
     private Long random(Long seed) {
